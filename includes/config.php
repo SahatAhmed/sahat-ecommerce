0e1c06790d2aea5d126d5a3f67c844e993241c94
SahatAhmed <warriorking111004@gmail.com>
2025-11-09 16:51:33 +0530

    Change DB_HOST from '127.0.0.1' to 'localhost'

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php
 // Edit these values for your environment
-define('DB_HOST','127.0.0.1');
+define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','ecommerce_db'); // DataBase Name
