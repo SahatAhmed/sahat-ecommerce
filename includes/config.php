95aeee6aa3602450ca3c1d1fceaedf81c7865061
SahatAhmed <warriorking111004@gmail.com>
2025-11-09 16:54:02 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,11 @@
 <?php
 // Edit these values for your environment
-define('DB_HOST','localhost');
+define('DB_HOST','127.0.0.1');
 define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','ecommerce_db'); // DataBase Name
 
 define('BASE_URL','/ecommerce/'); // relative to web root
 ?>
+
 
